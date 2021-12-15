45792c8794301f3e70bca639ca6fbccfb2fe4623
Shreesh Pandey <shvspandey@gmail.com>
2021-12-15 09:05:09 +0530

    Checking if Employee is Present or Absent

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# This is a sample Python script.
-# displaying Welcome Message on the Master Branch
-print("Welcome to The Employee Wage Computational Problem")
-
+# random function to generate random numbers
+import random
+# initialising the present employee value
+present_employee=1
+# generating the random number
+a=random.randint(0,1)
+if(a==1):
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
